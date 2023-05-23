b7c6296edab66337e0af45566a9ff3ec07f13106
Kreyno93 <michael.wagener@neuefische.de>
2023-05-23 17:15:54 +0200

    adding integrations tests for test purposes

---
+++ backend/src/test/java/de/neuefische/backend/CarControllerTest.java
@@ -0,0 +1,32 @@
+package de.neuefische.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockRequestDispatcher;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CarControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DirtiesContext
+    void getCar() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/cars"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
+    }
+
+}
